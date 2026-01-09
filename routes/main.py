ed4e5a957293f8b46c031e27373165afcab6c9da
casescope <casescope@casescope.local>
2026-01-09 14:54:31 +0000

    Add Case Selection page with table listing all cases

--- routes/main.py
+++ routes/main.py
@@ -1,6 +1,7 @@
 """Main routes for CaseScope"""
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
+from models.case import Case, CaseStatus
 
 main_bp = Blueprint('main', __name__)
 
@@ -17,3 +18,23 @@ def index():
 def dashboard():
     """Dashboard page"""
     return render_template('dashboard.html', page_title='Dashboard')
+
+
+@main_bp.route('/cases')
+@login_required
+def cases():
+    """Case Selection - list all cases"""
+    all_cases = Case.query.order_by(Case.created_at.desc()).all()
+    return render_template(
+        'cases.html',
+        page_title='Case Selection',
+        cases=all_cases,
+        CaseStatus=CaseStatus
+    )
+
+
+@main_bp.route('/cases/new')
+@login_required
+def case_create():
+    """Create new case form"""
+    return render_template('case_create.html', page_title='Create Case')
